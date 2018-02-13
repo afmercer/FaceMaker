2814f1d202a979eba1877c91ff20a629941a22a1
amercer98 <mercer20@up.edu>
2018-02-12 19:18:36 -0800

    Initial GUI Finished

--- app/src/main/java/edu/up/cs301/mercer20/facemaker/MainActivity.java
+++ app/src/main/java/edu/up/cs301/mercer20/facemaker/MainActivity.java
@@ -2,17 +2,21 @@ package edu.up.cs301.mercer20.facemaker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.SurfaceView;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
-    //private String[] hairStyles = {}
+    private String[] hairStyleNames = {"Bowl Cut", "Spiky", "Balding"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> hairAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, hairStyleNames);
+        Spinner hairStyles = (Spinner)findViewById(R.id.spinnerHairStyles);
+        hairStyles.setAdapter(hairAdapter);
     }
 }
